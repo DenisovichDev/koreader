89113018a9b38c48714523842b8c169bfa1bb717
DenisovichDev <bhaswarishi2908@gmail.com>
2025-12-22 13:57:52 +0530

    Remove randomquote plugin to reattach as subtree

--- plugins/randomquote.koplugin/_meta.lua
+++
@@ -1,9 +0,0 @@
--- SPDX-License-Identifier: AGPL-3.0-or-later
-
-return {
-    name = "randomquote",
-    fullname = "Random Quote on Lock",
-    version = "0.1.0",
-    author = "DenisovichDev",
-    description = "Shows a random quotation from my highlights when locked.",
-}
